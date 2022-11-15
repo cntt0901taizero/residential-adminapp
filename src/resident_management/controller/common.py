a1cf7d5fb04ffae50c057714ad067061ce37870b
taict <taict@abbank.vn>
2022-11-15 09:33:37 +0700

    taict api 15112022

--- src/resident_management/controller/common.py
+++ src/resident_management/controller/common.py
@@ -1,20 +1,21 @@
 import ast
-import json
+# import json
 import datetime
 import logging
-from werkzeug.wrappers import Request, Response
+from werkzeug.wrappers import json, Request, Response
 
 _logger = logging.getLogger(__name__)
 
 
 def common_response(status='404', message='', data=[]):
+    # headers_json = {'Content-Type': 'application/json'}
     response_data = {
         'status': status,
         'message': message,
         'data': data
     }
+    # return Response(json.dumps(response_data), headers=headers_json)
     return response_data
-
 
 def extract_arguments(limit="80", offset=0, order="id", domain="", fields=[]):
     """Parse additional data  sent along request."""
