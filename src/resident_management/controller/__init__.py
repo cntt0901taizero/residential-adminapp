02062c6060742aadf38dd9e5b64b2c8ec9dbe39e
NORTH\tiepnc <tiepngochi@gmail.com>
2022-11-16 08:43:59 +0700

    notification

--- src/resident_management/controller/__init__.py
+++ src/resident_management/controller/__init__.py
@@ -3,6 +3,7 @@ from . import feekind_controller
 from . import paybillconfigrender_controller
 from . import users_controller
 from . import authenticate_controller
+from . import notification_controller
 # from . import login
 
 
--- src/resident_management/models/tb_notification.py
+++ src/resident_management/models/tb_notification.py
@@ -1,19 +1,27 @@
 from odoo import models, fields
+from pyfcm import FCMNotification
 
-
+push_service = FCMNotification(
+    api_key="AAAAz6dhWnM:APA91bE2nkH_zcfTAlAuMkCLfnZ1m2y7zg_YMEmQnYBPkZ6JHpUQpNkYqh8f9vtRckFZX1Pl50aUXCbfni23b81OyMkDEPwnctsj4Sg9-IZx_tpgFVajvZMamtVz7_aZInJaRMtaGk_5")
+registration_id = 'dsaQ4hK8Tt2BiYcpstSQz7:APA91bGJHEkaWqlzOky-a9uNJzMhVa2f708tcYXLV-9IrXpqhqkDemI0UZ1uaSnV1TSlQjlCu54fO59AmN0P3ubYDBZDk9zaW8oIDLxTtDa1EzuPwnNUL0stkkPn1kutHOqmnOx7h0UB'
+message_title = "Uber update"
+message_body = "Hi john, your customized news for today is ready"
 class tb_notification(models.Model):
     _name = 'tb_notification'
     _description = 'Thông báo'
 
-    name = fields.Char(string='Tiêu đề', required=True, copy=False,)
-    content = fields.Char(string='Nội dung', required=True, copy=False,)
+    name = fields.Char(string='Tiêu đề', required=True, copy=False, )
+    content = fields.Text(string='Nội dung', required=True, copy=False, )
     type = fields.Selection([
         ('NEWS', 'Bản tin'),
         ('ACTIVE_BY_ADMIN', 'Thông báo từ quản trị viên'),
-    ], required=True, default='NEWS', tracking=True, string="Loại thông báo",)
+    ], required=True, default='NEWS', tracking=True, string="Loại thông báo", )
     state = fields.Selection([
         ('PENDING', 'Chờ duyệt'),
         ('ACTIVE', 'Đã duyệt'),
         ('REJECT', 'Chưa duyệt'),
     ], required=True, default='PENDING', tracking=True, string="Trạng thái", )
-
+    def set_status_active(self):
+        self.write({'state': 'ACTIVE'})
+    def set_status_reject(self):
+        self.write({'state': 'REJECT'})
