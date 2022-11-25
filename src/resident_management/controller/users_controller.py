54f90348d48c29da85bb6fb2279a795d068c83e8
taict <taict@abbank.vn>
2022-11-25 09:04:49 +0700

    taict commit 25112022

--- src/resident_management/controller/users_controller.py
+++ src/resident_management/controller/users_controller.py
@@ -9,24 +9,37 @@ import xmlrpc.client
 
 class Users_Controller(http.Controller):
 
-    @http.route('/auth/login/email', auth="public", methods=["POST"], type='json', cors='*', csrf=False)
-    def users_login_email(self, **kwargs):
-        url = 'http://localhost:8069/'
-        db_name = 'my_db_1'
-        common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-        common.version()
-        print("ui id")
-        return "123"
-
     @http.route('/api/users/user-info', methods=['GET'], auth='user', type='json', cors='*', csrf=False)
     def user_info(self, *args, **kwargs):
         try:
             user = request.env.user
             sid = request.session.sid
-            # reports = request.env['res.users'].search([('id', '=', self_id)])
             base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
             image_url = base_url + '/web/image?' + 'model=res.users&id=' + str(
                 user.id) + '&field=avatar_1920' if user.avatar_1920 else None
+            data = {
+                'id': user.id,
+                'sid': sid,
+                'login': user.login,
+                'email': user.email,
+                'phone': user.phone,
+                'display_name': user.display_name,
+                'image_url': image_url,
+                'signature': user.signature,
+                'active': user.active
+            }
+            return common_response('200', '', data)
+        except Exception as e:
+            return common_response('500', e.name, [])
+
+    @http.route('/api/users/user-by-id/{self_id}', methods=['GET'], auth='none', type='json', cors='*', csrf=False)
+    def user_info(self, self_id, **kwargs):
+        try:
+            user = request.env['res.users'].search([('id', '=', self_id)])
+            base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
+            image_url = base_url + '/web/image?' + 'model=res.users&id=' + str(
+                user.id) + '&field=avatar_1920' if user.avatar_1920 else None
+            sid = request.session.sid
             data = {
                 'id': user.id,
                 'sid': sid,
