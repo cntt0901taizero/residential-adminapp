88f95638438d4896355995abffa1df43677cd8e1
NORTH\tiepnc <tiepngochi@gmail.com>
2022-11-25 09:06:03 +0700

    banner management

--- src/resident_management/models/__init__.py
+++ src/resident_management/models/__init__.py
@@ -7,6 +7,7 @@ from . import res_partner
 from . import groups_inherit
 from . import tb_notification
 from . import tb_users
+from . import tb_banner
 # from . import common
 # from . import access_token
 # from odoo.addons.resident_management.models.blockhouse_model import tb_building_house_detail
