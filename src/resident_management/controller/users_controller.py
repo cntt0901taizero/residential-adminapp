155de6345b63f33b88d33a1003ca1d1c42c9e873
anbinh-dbd <thetaichu@gmail.com>
2022-12-06 09:15:38 +0700

    taict commit

--- src/resident_management/controller/users_controller.py
+++ src/resident_management/controller/users_controller.py
@@ -35,22 +35,8 @@ class Users_Controller(http.Controller):
     def image_upload(self, *args, **kwargs):
         try:
             user = request.env.user
-            sid = request.session.sid
-            base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
-            image_url = base_url + '/web/image?' + 'model=res.users&id=' + str(
-                user.id) + '&field=avatar_1920' if user.avatar_1920 else None
-            data = {
-                'id': user.id,
-                'sid': sid,
-                'login': user.login,
-                'email': user.email,
-                'phone': user.phone,
-                'display_name': user.display_name,
-                'image_url': image_url,
-                'signature': user.signature,
-                'active': user.active
-            }
-            return common_response(200, '', data)
+
+            return common_response(200, '', [])
         except Exception as e:
             return common_response(500, e.name, [])
 
