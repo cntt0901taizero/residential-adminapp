4830d1b8f4d371edbda49ae7e4b54aef21540f02
Tiep Ngo Chi <tiepnc@abbank.vn>
2022-12-16 08:51:17 +0700

    push

--- src/resident_management/models/tb_users_blockhouse_res_groups_rel.py
+++ src/resident_management/models/tb_users_blockhouse_res_groups_rel.py
@@ -1,15 +1,19 @@
 from odoo import models, fields, api
 
+
 class tb_users_blockhouse_res_groups_rel(models.Model):
     _name = 'tb_users_blockhouse_res_groups_rel'
 
+    group_id = fields.Many2one(comodel_name='res.groups', string='Nhóm người dùng')
     name = fields.Selection([
         ('ADMINISTRATION', 'Ban quản trị'),
         ('MANAGEMENT', 'Ban quản lý'),
         ('RESIDENT', 'Cư dân'),
-    ], required=True, default='RESIDENT', string="Nhóm người dùng", )
-    user_id = fields.Many2one(comodel_name='res_user')
+    ], required=True, default='RESIDENT', string="Nhóm người dùng",)
+    user_id = fields.Many2one(comodel_name='res.users')
     blockhouse_id = fields.Many2one(comodel_name='tb_blockhouse', string='Khối nhà')
     building_id = fields.Many2one(comodel_name='tb_building', string='Tòa nhà')
     building_house_id = fields.Many2one(comodel_name='tb_building_house', string='Căn hộ')
     owner = fields.Boolean(string='Chủ sở hữu', default=False)
+
+
