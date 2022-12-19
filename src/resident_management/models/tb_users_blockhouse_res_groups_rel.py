7150795be98c385d473812e23a8e51f70ab496ea
Tiep Ngo Chi <tiepnc@abbank.vn>
2022-12-19 08:36:49 +0700

    push

--- src/resident_management/models/tb_users_blockhouse_res_groups_rel.py
+++ src/resident_management/models/tb_users_blockhouse_res_groups_rel.py
@@ -14,11 +14,17 @@ class tb_users_blockhouse_res_groups_rel(models.Model):
     ], required=True, default='RESIDENT', string="Nhóm người dùng", )
     user_id = fields.Many2one(comodel_name='res.users')
     blockhouse_id = fields.Many2one(comodel_name='tb_blockhouse', string='Khối nhà')
-    building_id = fields.Many2one(comodel_name='tb_building', string='Tòa nhà', domain="[('blockhouse_id', '=', blockhouse_id)]")
-    building_house_id = fields.Many2one(comodel_name='tb_building_house', string='Căn hộ', domain="[('building_id', '=', building_id)]")
+    building_id = fields.Many2one(comodel_name='tb_building', string='Tòa nhà',
+                                  domain="[('blockhouse_id', '=', blockhouse_id)]")
+    building_house_id = fields.Many2one(comodel_name='tb_building_house', string='Căn hộ',
+                                        domain="[('building_id', '=', building_id)]")
     owner = fields.Boolean(string='Chủ sở hữu', default=False)
 
-    @api.onchange('datas')
-    def _on_change_blockhouse_id(selfs):
-        selfs.building_id = None
-        selfs.building_house_id = None
+    @api.onchange('blockhouse_id')
+    def _on_change_blockhouse_id(self):
+        self.building_id = None
+        self.building_house_id = None
+
+    @api.onchange('building_id')
+    def _on_change_building_id(self):
+        self.building_house_id = None
