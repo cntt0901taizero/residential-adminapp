3841c4efdcf78f57b2e1c347ad1607278223113b
Tiep Ngo Chi <tiepnc@abbank.vn>
2023-01-10 14:25:18 +0700

    push

--- src/resident_management/__manifest__.py
+++ src/resident_management/__manifest__.py
@@ -4,7 +4,7 @@
     'category': 'Resident',
     'summary': 'RESIDENTIAL MANAGEMENT',
     'description': """RESIDENTIAL MANAGEMENT""",
-    'depends': ['base', 'mail', 'apartment_project'],
+    'depends': ['base', 'mail', 'apartment_project', 'apartment_notifications'],
     'sequence': -100,
     'data': [
         'views/icons.xml',
