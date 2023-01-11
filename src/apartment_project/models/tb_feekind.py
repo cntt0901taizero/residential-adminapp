110b8eb5b9bcd70b524630f8d9c3646c33e20e54
anbinh-dbd <thetaichu@gmail.com>
2023-01-11 16:50:41 +0700

    taict db 3

--- src/apartment_project/models/tb_feekind.py
+++ src/apartment_project/models/tb_feekind.py
@@ -12,6 +12,9 @@ class tb_feekind(models.Model):
     note = fields.Text(string='Ghi chú', copy=False, help='Ghi rõ ràng các khoản phí (nếu có)')
     is_active = fields.Boolean(string='Trạng thái', default=True)
     blockhouse_id = fields.Many2one(comodel_name='tb_blockhouse', string="Dự án", ondelet="cascade")
+    building_id = fields.Many2one(comodel_name='tb_building', string="Tòa nhà",
+                                  domain="[('blockhouse_id', '=', blockhouse_id)]",
+                                  ondelet="cascade")
 
     def set_status_active(self):
         self.is_active = True
