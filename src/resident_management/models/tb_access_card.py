b8982a9cdaf2fae3e91be838b1ffb1871a271815
anbinh-dbd <thetaichu@gmail.com>
2023-02-07 09:03:48 +0700

    taict upd

---
+++ src/resident_management/models/tb_access_card.py
@@ -0,0 +1,158 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from odoo.http import request
+
+from odoo.addons.resident_management.models.tb_users_blockhouse_res_groups_rel import USER_GROUP_CODE, RELATIONSHIP_TYPES
+
+str_bql = USER_GROUP_CODE[2][0]
+str_bqt = USER_GROUP_CODE[3][0]
+
+
+class tb_access_card(models.Model):
+    _name = 'tb_access_card'
+    _description = 'Thẻ ra vào'
+
+    name = fields.Char(string='Họ và tên', copy=False)
+    code = fields.Char(string='Mã', copy=False)
+    shipping_method = fields.Selection(string='Quan hệ', selection=RELATIONSHIP_TYPES,
+                                       default=RELATIONSHIP_TYPES[0][0])
+    status = fields.Boolean(string='Trạng thái', default=True)
+
+    blockhouse_id = fields.Many2one(comodel_name='tb_blockhouse', string="Dự án",
+                                    domain=lambda self: self._domain_blockhouse_id(),
+                                    ondelete="cascade")
+    building_id = fields.Many2one(comodel_name='tb_building', string="Toà nhà",
+                                  domain="[('blockhouse_id', '!=', None), ('blockhouse_id', '=', blockhouse_id)]",
+                                  ondelete="cascade")
+    building_house_id = fields.Many2one(comodel_name='tb_building_house', string="Căn hộ",
+                                        domain="[('building_id', '!=', None), ('building_id', '=', building_id)]",
+                                        ondelete="cascade")
+    user_id = fields.Many2one(comodel_name='res.users', string="Chủ sở hữu",
+                              domain=lambda self: self._domain_user_id(),
+                              ondelete="cascade")
+
+    @api.model
+    def _domain_user_id(self):
+        user = request.env.user
+        bqt_bh_id = []  # ban quan tri - blockhouse - id
+        bqt_bd_id = []  # ban quan tri - building - id
+        bql_bh_id = []  # ban quan ly - blockhouse - id
+        bql_bd_id = []  # ban quan ly - building - id
+        if user and user.id != 1 and user.id != 2:
+            for item in user.tb_users_blockhouse_res_groups_rel_ids:
+                if item.group_id.name and str_bqt in item.user_group_code:
+                    bqt_bh_id.append(int(item.blockhouse_id.id))
+                    bqt_bd_id.append(int(item.building_id.id))
+                if item.group_id.name and str_bql in item.user_group_code:
+                    bql_bh_id.append(int(item.blockhouse_id.id))
+                    bql_bd_id.append(int(item.building_id.id))
+
+            user_ids_1 = self.env['tb_users_blockhouse_res_groups_rel'].sudo()\
+                .search([('building_id', 'in', list(set(bqt_bd_id + bql_bd_id)))])
+            user_ids_2 = self.env['tb_users_blockhouse_res_groups_rel'].sudo()\
+                .search([('blockhouse_id', 'in', list(set(bqt_bh_id + bql_bh_id)))])
+
+            return ["&", ("active", "=", True), ("id", "in", list(set(user_ids_1 + user_ids_2)))]
+        else:
+            return [("active", "=", True)]
+
+    @api.model
+    def _domain_blockhouse_id(self):
+        user = request.env.user
+        bqt_bh_id = []  # ban quan tri - blockhouse - id
+        bqt_bd_id = []  # ban quan tri - building - id
+        bql_bh_id = []  # ban quan ly - blockhouse - id
+        bql_bd_id = []  # ban quan ly - building - id
+        if user and user.id != 1 and user.id != 2:
+            for item in user.tb_users_blockhouse_res_groups_rel_ids:
+                if item.group_id.name and str_bqt in item.user_group_code:
+                    bqt_bh_id.append(int(item.blockhouse_id.id))
+                    bqt_bd_id.append(int(item.building_id.id))
+                if item.group_id.name and str_bql in item.user_group_code:
+                    bql_bh_id.append(int(item.blockhouse_id.id))
+                    bql_bd_id.append(int(item.building_id.id))
+            return ["&", ("is_active", "=", True), ("id", "in", list(set(bqt_bh_id + bql_bh_id)))]
+        else:
+            return [("is_active", "=", True)]
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        user = request.env.user
+        bqt_bh_id = []  # ban quan tri - blockhouse - id
+        bqt_bd_id = []  # ban quan tri - building - id
+        bql_bh_id = []  # ban quan ly - blockhouse - id
+        bql_bd_id = []  # ban quan ly - building - id
+        if user and user.id != 1 and user.id != 2:
+            for item in user.tb_users_blockhouse_res_groups_rel_ids:
+                if item.group_id.name and str_bqt in item.user_group_code:
+                    bqt_bh_id.append(int(item.blockhouse_id.id))
+                    bqt_bd_id.append(int(item.building_id.id))
+                if item.group_id.name and str_bql in item.user_group_code:
+                    bql_bh_id.append(int(item.blockhouse_id.id))
+                    bql_bd_id.append(int(item.building_id.id))
+            domain.append(('building_id', 'in', list(set(bqt_bd_id + bql_bd_id))))
+            domain.append(('blockhouse_id', 'in', list(set(bqt_bh_id + bql_bh_id))))
+        res = super(tb_access_card, self).read_group(domain, fields, groupby, offset=offset, limit=limit,
+                                                 orderby=orderby, lazy=lazy)
+        return res
+
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=10, order=None):
+        user = request.env.user
+        bqt_bh_id = []  # ban quan tri - blockhouse - id
+        bqt_bd_id = []  # ban quan tri - building - id
+        bql_bh_id = []  # ban quan ly - blockhouse - id
+        bql_bd_id = []  # ban quan ly - building - id
+        if user and user.id != 1 and user.id != 2:
+            for item in user.tb_users_blockhouse_res_groups_rel_ids:
+                if item.group_id.name and str_bqt in item.user_group_code:
+                    bqt_bh_id.append(int(item.blockhouse_id.id))
+                    bqt_bd_id.append(int(item.building_id.id))
+                if item.group_id.name and str_bql in item.user_group_code:
+                    bql_bh_id.append(int(item.blockhouse_id.id))
+                    bql_bd_id.append(int(item.building_id.id))
+            domain.append(('building_id', 'in', list(set(bqt_bd_id + bql_bd_id))))
+            domain.append(('blockhouse_id', 'in', list(set(bqt_bh_id + bql_bh_id))))
+        res = super(tb_access_card, self).search_read(domain, fields, offset, limit, order)
+        return res
+
+    def open_edit_form(self):
+        can_do = self.check_access_rights('write', raise_exception=False)
+        if not can_do:
+            raise ValidationError('Bạn không có quyền chỉnh sửa thông tin!')
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Sửa: ' + self.name,
+            'res_model': 'tb_access_card',
+            'res_id': self.id,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': self.env.ref('apartment_service_support.view_tb_access_card_form').id,
+            'context': {'form_view_initial_mode': 'edit'},
+            'target': 'current',
+        }
+
+    def confirm_delete(self):
+        candelete = self.check_access_rights('unlink', raise_exception=False)
+        if not candelete:
+            raise ValidationError('Bạn không có quyền xóa bản ghi này!')
+        message = """Bạn có chắc muốn xóa bản ghi này?"""
+        value = self.env['dialog.box.confirm'].sudo().create({'message': message})
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Xóa bản ghi',
+            'res_model': 'dialog.box.confirm',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'new',
+            'res_id': value.id
+        }
+
+    def del_record(self):
+        for record in self:
+            record.unlink()
+            pass
+
+
+
+
---
+++ src/resident_management/models/tb_complain.py
@@ -0,0 +1,153 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from odoo.http import request
+
+from odoo.addons.resident_management.models.tb_users_blockhouse_res_groups_rel import USER_GROUP_CODE
+str_bql = USER_GROUP_CODE[2][0]
+str_bqt = USER_GROUP_CODE[3][0]
+
+
+class tb_complain(models.Model):
+    _name = 'tb_complain'
+    _description = 'Phương tiện'
+
+    name = fields.Char(string='Tiêu đề', copy=False)
+    content = fields.Html(string='Nội dung', copy=False)
+    file = fields.Binary(string='Tài liệu', attachment=True, help='Chọn tài liệu tải lên')
+    status_description = fields.Html(string='Người xử lý ghi chú', copy=False)
+    status = fields.Boolean(string='Trạng thái', default=True)
+    blockhouse_id = fields.Many2one(comodel_name='tb_blockhouse', string="Dự án",
+                                    domain=lambda self: self._domain_blockhouse_id(),
+                                    ondelete="cascade")
+    building_id = fields.Many2one(comodel_name='tb_building', string="Toà nhà",
+                                  domain="[('blockhouse_id', '!=', None), ('blockhouse_id', '=', blockhouse_id)]",
+                                  ondelete="cascade")
+    user_id = fields.Many2one(comodel_name='res.users', string="Chủ sở hữu",
+                              domain=lambda self: self._domain_user_id(),
+                              ondelete="cascade")
+
+    @api.model
+    def _domain_user_id(self):
+        user = request.env.user
+        bqt_bh_id = []  # ban quan tri - blockhouse - id
+        bqt_bd_id = []  # ban quan tri - building - id
+        bql_bh_id = []  # ban quan ly - blockhouse - id
+        bql_bd_id = []  # ban quan ly - building - id
+        if user and user.id != 1 and user.id != 2:
+            for item in user.tb_users_blockhouse_res_groups_rel_ids:
+                if item.group_id.name and str_bqt in item.user_group_code:
+                    bqt_bh_id.append(int(item.blockhouse_id.id))
+                    bqt_bd_id.append(int(item.building_id.id))
+                if item.group_id.name and str_bql in item.user_group_code:
+                    bql_bh_id.append(int(item.blockhouse_id.id))
+                    bql_bd_id.append(int(item.building_id.id))
+
+            user_ids_1 = self.env['tb_users_blockhouse_res_groups_rel'].sudo()\
+                .search([('building_id', 'in', list(set(bqt_bd_id + bql_bd_id)))])
+            user_ids_2 = self.env['tb_users_blockhouse_res_groups_rel'].sudo()\
+                .search([('blockhouse_id', 'in', list(set(bqt_bh_id + bql_bh_id)))])
+
+            return ["&", ("active", "=", True), ("id", "in", list(set(user_ids_1 + user_ids_2)))]
+        else:
+            return [("active", "=", True)]
+
+    @api.model
+    def _domain_blockhouse_id(self):
+        user = request.env.user
+        bqt_bh_id = []  # ban quan tri - blockhouse - id
+        bqt_bd_id = []  # ban quan tri - building - id
+        bql_bh_id = []  # ban quan ly - blockhouse - id
+        bql_bd_id = []  # ban quan ly - building - id
+        if user and user.id != 1 and user.id != 2:
+            for item in user.tb_users_blockhouse_res_groups_rel_ids:
+                if item.group_id.name and str_bqt in item.user_group_code:
+                    bqt_bh_id.append(int(item.blockhouse_id.id))
+                    bqt_bd_id.append(int(item.building_id.id))
+                if item.group_id.name and str_bql in item.user_group_code:
+                    bql_bh_id.append(int(item.blockhouse_id.id))
+                    bql_bd_id.append(int(item.building_id.id))
+            return ["&", ("is_active", "=", True), ("id", "in", list(set(bqt_bh_id + bql_bh_id)))]
+        else:
+            return [("is_active", "=", True)]
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        user = request.env.user
+        bqt_bh_id = []  # ban quan tri - blockhouse - id
+        bqt_bd_id = []  # ban quan tri - building - id
+        bql_bh_id = []  # ban quan ly - blockhouse - id
+        bql_bd_id = []  # ban quan ly - building - id
+        if user and user.id != 1 and user.id != 2:
+            for item in user.tb_users_blockhouse_res_groups_rel_ids:
+                if item.group_id.name and str_bqt in item.user_group_code:
+                    bqt_bh_id.append(int(item.blockhouse_id.id))
+                    bqt_bd_id.append(int(item.building_id.id))
+                if item.group_id.name and str_bql in item.user_group_code:
+                    bql_bh_id.append(int(item.blockhouse_id.id))
+                    bql_bd_id.append(int(item.building_id.id))
+            domain.append(('building_id', 'in', list(set(bqt_bd_id + bql_bd_id))))
+            domain.append(('blockhouse_id', 'in', list(set(bqt_bh_id + bql_bh_id))))
+        res = super(tb_complain, self).read_group(domain, fields, groupby, offset=offset, limit=limit,
+                                                 orderby=orderby, lazy=lazy)
+        return res
+
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=10, order=None):
+        user = request.env.user
+        bqt_bh_id = []  # ban quan tri - blockhouse - id
+        bqt_bd_id = []  # ban quan tri - building - id
+        bql_bh_id = []  # ban quan ly - blockhouse - id
+        bql_bd_id = []  # ban quan ly - building - id
+        if user and user.id != 1 and user.id != 2:
+            for item in user.tb_users_blockhouse_res_groups_rel_ids:
+                if item.group_id.name and str_bqt in item.user_group_code:
+                    bqt_bh_id.append(int(item.blockhouse_id.id))
+                    bqt_bd_id.append(int(item.building_id.id))
+                if item.group_id.name and str_bql in item.user_group_code:
+                    bql_bh_id.append(int(item.blockhouse_id.id))
+                    bql_bd_id.append(int(item.building_id.id))
+            domain.append(('building_id', 'in', list(set(bqt_bd_id + bql_bd_id))))
+            domain.append(('blockhouse_id', 'in', list(set(bqt_bh_id + bql_bh_id))))
+        res = super(tb_complain, self).search_read(domain, fields, offset, limit, order)
+        return res
+
+    def open_edit_form(self):
+        can_do = self.check_access_rights('write', raise_exception=False)
+        if not can_do:
+            raise ValidationError('Bạn không có quyền chỉnh sửa thông tin!')
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Sửa phương tiện ' + self.name,
+            'res_model': 'tb_complain',
+            'res_id': self.id,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': self.env.ref('apartment_service_support.view_tb_complain_form').id,
+            'context': {'form_view_initial_mode': 'edit'},
+            'target': 'current',
+        }
+
+    def confirm_delete(self):
+        candelete = self.check_access_rights('unlink', raise_exception=False)
+        if not candelete:
+            raise ValidationError('Bạn không có quyền xóa bản ghi này!')
+        message = """Bạn có chắc muốn xóa bản ghi này?"""
+        value = self.env['dialog.box.confirm'].sudo().create({'message': message})
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Xóa bản ghi',
+            'res_model': 'dialog.box.confirm',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'new',
+            'res_id': value.id
+        }
+
+    def del_record(self):
+        for record in self:
+            record.unlink()
+            pass
+
+
+
+
